d4d34fe2ae60c1d368dd080c565164242629c0d8
Pavel <pavel.mokr2005@gmail.com>
2025-03-12 22:49:16 +0500

    unsuccessful outline

---
+++ Assets/Terrix/Shaders/JFA/JFA_function.hlsl
@@ -0,0 +1,46 @@
+﻿//UNITY_SHADER_NO_UPGRADE
+#ifndef MYHLSLINCLUDE_INCLUDED
+#define MYHLSLINCLUDE_INCLUDED
+
+bool IsUVOutOfRange(float2 uv)
+{
+    return any(uv < 0) || any(uv > 1);
+}
+
+void JFA_float(float2 UV, UnityTexture2D Tex, float StepSize, out float4 Out)
+{
+    float bestDistance = 99999.0;
+    float3 bestPos = float3(0, 0, 1);
+    
+    for (int y = -1; y <= 1; y++)
+    {
+        for (int x = -1; x <= 1; x++)
+        {
+            float2 UV_off = UV + float2(x, y) * StepSize;
+
+            if (IsUVOutOfRange(UV_off))
+            {
+                continue;
+            }
+
+            
+            float3 pos = tex2D(Tex, UV_off).xyz;
+
+            if (pos.z == 1)
+            {
+                continue;
+            }
+            
+            float dist = distance(pos.xy, UV);
+            
+            if (dist < bestDistance)
+            {
+                bestDistance = dist;
+                bestPos = pos;
+            }
+        }
+    }
+
+    Out = float4(bestPos, 0);
+}
+#endif //MYHLSLINCLUDE_INCLUDED
---
+++ Assets/Terrix/Shaders/JFA/OutlineRenderFeature.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.RenderGraphModule.Util;
+using UnityEngine.Rendering.Universal;
+
+namespace Terrix.Shaders.JFA
+{
+    public class OutlineRenderFeature : ScriptableRendererFeature
+    {
+        [SerializeField] private RenderPassEvent inject;
+
+        [SerializeField] private Shader jfaInit;
+        [SerializeField] private Shader jfaPass;
+        [SerializeField] private Shader jfaOutline;
+        [SerializeField] private LayerMask targetLayers;
+        [SerializeField, Min(0)] private float outlinePixelWidth = 2f;
+
+        private OutlineRenderPass outlineRenderPass;
+
+        public override void Create()
+        {
+            outlineRenderPass = new OutlineRenderPass
+            {
+                renderPassEvent = inject
+            };
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (jfaInit == null)
+            {
+                Debug.LogWarning($"{nameof(jfaInit)} is null {nameof(OutlineRenderPass)} skipped...");
+                return;
+            }
+
+            if (jfaPass == null)
+            {
+                Debug.LogWarning($"{nameof(jfaPass)} is null {nameof(OutlineRenderPass)} skipped...");
+                return;
+            }
+            
+            if (jfaOutline == null)
+            {
+                Debug.LogWarning($"{nameof(jfaOutline)} is null {nameof(OutlineRenderPass)} skipped...");
+                return;
+            }
+
+            outlineRenderPass.Setup(
+                jfaInit,
+                jfaPass,
+                jfaOutline,
+                outlinePixelWidth,
+                targetLayers);
+
+            renderer.EnqueuePass(outlineRenderPass);
+        }
+
+        private class OutlineRenderPass : ScriptableRenderPass
+        {
+            private Shader jfaInitShader;
+            private Shader jfaPassShader;
+            private Shader jfaOutlineShader;
+            
+            private float outlinePixelWidth;
+
+            private FilteringSettings filteringSettings;
+
+            readonly List<ShaderTagId> shaderTagIds = new()
+            {
+                new ShaderTagId("UniversalForward"),
+                new ShaderTagId("SRPDefaultUnlit")
+            };
+
+
+            private class FillMaskPassData
+            {
+                public RendererListHandle maskedObjects;
+            }
+
+            public void Setup(Shader jfaInit, Shader jfaPass, Shader jfaOutline ,float outlineWidth, LayerMask targetLayers)
+            {
+                this.outlinePixelWidth = outlineWidth;
+                this.jfaInitShader = jfaInit;
+                this.jfaPassShader = jfaPass;
+                this.jfaOutlineShader = jfaOutline;
+                this.filteringSettings = new FilteringSettings(RenderQueueRange.all, layerMask: targetLayers);
+            }
+
+            public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+            {
+                var resourceData = frameData.Get<UniversalResourceData>();
+
+                if (resourceData.isActiveTargetBackBuffer)
+                {
+                    return;
+                }
+
+                var renderingData = frameData.Get<UniversalRenderingData>();
+                var cameraData = frameData.Get<UniversalCameraData>();
+                var lightData = frameData.Get<UniversalLightData>();
+
+                var bufferTextureDescriptor = new RenderTextureDescriptor(
+                    cameraData.cameraTargetDescriptor.width,
+                    cameraData.cameraTargetDescriptor.height,
+                    cameraData.cameraTargetDescriptor.graphicsFormat,
+                    0
+                );
+                var bufferTextureDescriptor2 = new RenderTextureDescriptor(
+                    cameraData.cameraTargetDescriptor.width,
+                    cameraData.cameraTargetDescriptor.height,
+                    cameraData.cameraTargetDescriptor.graphicsFormat,
+                    0
+                );
+
+                var ping = UniversalRenderer.CreateRenderGraphTexture(renderGraph, bufferTextureDescriptor, "Buffer 1", true);
+                var pong = UniversalRenderer.CreateRenderGraphTexture(renderGraph, bufferTextureDescriptor2, "Buffer 2", true);
+
+                using (var builder =
+                       renderGraph.AddRasterRenderPass<FillMaskPassData>("Fill Mask Pass", out var passData))
+                {
+                    var drawSettings = RenderingUtils.CreateDrawingSettings(
+                        shaderTagIds,
+                        renderingData,
+                        cameraData,
+                        lightData,
+                        cameraData.defaultOpaqueSortFlags
+                    );
+
+                    var rendererListParameters =
+                        new RendererListParams(renderingData.cullResults, drawSettings, filteringSettings);
+                    passData.maskedObjects = renderGraph.CreateRendererList(rendererListParameters);
+
+                    builder.SetRenderAttachment(ping, 0);
+                    builder.UseRendererList(passData.maskedObjects);
+
+                    builder.SetRenderFunc((FillMaskPassData data, RasterGraphContext context) =>
+                    {
+                        context.cmd.ClearRenderTarget(true, true, Color.clear);
+                        context.cmd.DrawRendererList(data.maskedObjects);
+                    });
+                }
+
+                var jfaInitPass =
+                    new RenderGraphUtils.BlitMaterialParameters(ping, pong, new Material(jfaInitShader), 0)
+                    {
+                        sourceTexturePropertyID = Shader.PropertyToID("_MainTex")
+                    };
+                renderGraph.AddBlitPass(jfaInitPass, "JFA Init Pass");
+
+                var maxTextureSize = Mathf.Max(
+                    cameraData.cameraTargetDescriptor.width,
+                    cameraData.cameraTargetDescriptor.height);
+
+                var numMips = Mathf.CeilToInt(Mathf.Log(maxTextureSize / 2, 2f));
+
+                for (var i = 1; i <= numMips; i++)
+                {
+                    var stepWidth = 1 / Mathf.Pow(2, i);
+                    var jfaPassMaterial = new Material(jfaPassShader);
+                    jfaPassMaterial.SetFloat(Shader.PropertyToID("_StepSize"), stepWidth);
+                    var jfaPass = new RenderGraphUtils.BlitMaterialParameters(pong, ping, jfaPassMaterial, 0)
+                    {
+                        sourceTexturePropertyID = Shader.PropertyToID("_MainTex")
+                    };
+                    renderGraph.AddBlitPass(jfaPass, "JFA Pass");
+                    (ping, pong) = (pong, ping);
+                }
+
+                var jfaOutline = new RenderGraphUtils.BlitMaterialParameters(pong, ping, new Material(jfaOutlineShader), 0)
+                {
+                    sourceTexturePropertyID = Shader.PropertyToID("_MainTex")
+                };
+                renderGraph.AddBlitPass(jfaOutline, "JFA Outline");
+                
+                resourceData.cameraColor = ping;
+            }
+        }
+    }
+}
